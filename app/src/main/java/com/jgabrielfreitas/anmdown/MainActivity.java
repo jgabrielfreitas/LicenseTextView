a94248ebb8cd5169d6c5874765b4667443d16b9c
JGabrielFreitas <jgabrielafreitas@gmail.com>
2016-04-22 15:39:03 -0300

    Initial commit

---
+++ app/src/main/java/com/jgabrielfreitas/anmdown/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jgabrielfreitas.anmdown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
